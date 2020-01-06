f54cb5511c469a7b469d955f38304c8972c6da6d
MikesoWeb <klionpsyx@gmail.com>
2020-01-06 17:29:47 +0300

    add new program

---
+++ list_children.py
@@ -0,0 +1,8 @@
+# coding: utf8
+
+lst_new_children = {1: 'Маша Пупкина,',
+                    2: 'Vera Hrenova,',
+                    3: 'Оля Пяточкова,'
+                    }
+
+print(lst_new_children)
